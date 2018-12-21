94eb9fe47449e424c46add85b3b2fca9f5ab9680
Keith Ferney <keith@ferney.org>
2018-12-21 12:58:58 -0700

    travis-ci fix

--- cucumber.sh
+++ cucumber.sh
@@ -1,5 +1,5 @@
 bundle exec rails db:environment:set RAILS_ENV=test
-xvfb-run -a bundle exec cucumber RAILS_ENV=test $1
+xvfb-run -a bundle exec cucumber $1 RAILS_ENV=test
 EXIT_CODE=$?
 rm -rf /tmp/.X*-lock
 exit $EXIT_CODE
