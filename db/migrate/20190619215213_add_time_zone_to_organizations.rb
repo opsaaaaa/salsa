b24f90e04964b426c08cf5c2b78e0cfde7cd66cd
opsaaaaa <sean@ferney.org>
2019-06-24 13:32:02 -0600

    removed default UTC from migrations

--- db/migrate/20190619215213_add_time_zone_to_organizations.rb
+++ db/migrate/20190619215213_add_time_zone_to_organizations.rb
@@ -1,5 +1,5 @@
 class AddTimeZoneToOrganizations < ActiveRecord::Migration[5.1]
   def change
-    add_column :organizations, :time_zone, :string
+    add_column :organizations, :time_zone, :string, default: nil
   end
 end
--- db/migrate/20190619221312_change_organizations_time_zone_default.rb
+++
@@ -1,5 +0,0 @@
-class ChangeOrganizationsTimeZoneDefault < ActiveRecord::Migration[5.1]
-  def change
-    change_column_default :organizations, :time_zone, "UTC"
-  end
-end
