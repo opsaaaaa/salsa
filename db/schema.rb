7edf24b72bc0ccbf702a3842f4869598e009e1f6
opsaaaaa <sean@ferney.org>
2019-07-24 15:04:54 -0600

    added reports_include_lms to organization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190712081735) do
+ActiveRecord::Schema.define(version: 20190724202733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -153,6 +153,8 @@ ActiveRecord::Schema.define(version: 20190712081735) do
     t.string "authn_context"
     t.string "lms_account_id"
     t.string "period_meta_key"
+    t.string "time_zone"
+    t.string "reports_include_lms", default: "false"
     t.index ["depth"], name: "index_organizations_on_depth"
     t.index ["lft"], name: "index_organizations_on_lft"
     t.index ["lms_id"], name: "index_organizations_on_lms_id"
@@ -194,7 +196,6 @@ ActiveRecord::Schema.define(version: 20190712081735) do
     t.datetime "updated_at"
     t.json "report_filters"
     t.boolean "is_archived", default: false
-    t.string "archive_url"
     t.index ["organization_id"], name: "index_report_archives_on_organization_id"
   end
 
@@ -284,4 +285,5 @@ ActiveRecord::Schema.define(version: 20190712081735) do
     t.string "step_type", default: "default_step"
   end
 
+  add_foreign_key "organizations", "organizations", column: "parent_id"
 end
