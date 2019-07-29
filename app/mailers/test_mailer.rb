82c6d00b097037a3989f94efa694c72a40fd2dbb
John Pope <john@pope.tech>
2019-07-29 01:19:11 -0600

    html safe

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -3,7 +3,7 @@ class TestMailer < ApplicationMailer
   layout 'mailer'
 
   def test_email email, subject
-    @test_email = '<h1>TEST</h1>This is a test email from Salsa'
+    @test_email = '<h1>TEST</h1>This is a test email from Salsa'.html_safe
     mail(to: email, subject: subject)
   end
 end
