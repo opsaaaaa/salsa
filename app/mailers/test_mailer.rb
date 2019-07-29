7d0362de1465523249ac5d7ef82d50aafe29659e
John Pope <john@pope.tech>
2019-07-29 00:46:11 -0600

    Fix typo

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,4 +1,4 @@
-class ApplicationMailer < ActionMailer::Base
+class TestMailer < ActionMailer::Base
   def send_test email, subject
     @send_test = '<h1>TEST</h1>This is a test email from Salsa'
     send_email(to: email, subject: subject)
