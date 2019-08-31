ac42baa5a36d4283c01fcf0ef150afe5ad01b268
John Pope <john@pope.tech>
2019-08-30 18:30:50 -0600

    fix supervisor views

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -34,7 +34,7 @@ class AdminController < ApplicationController
       return redirect_to admin_unassigned_user_path
     end
     if has_role 'designer' or has_role 'supervisor'
-      redirect_to organizations_path, notice: flash[:notice]
+      redirect_to organizations_path(org_path: get_org&.path), notice: flash[:notice]
     elsif has_role 'auditor'
       redirect_to admin_auditor_reports_path, notice: flash[:notice]
     elsif ( has_role('staff', assignment_org = get_user_assignment_org(session[:authenticated_user],'staff')) || has_role('approver', assignment_org = get_user_assignment_org(session[:authenticated_user],'approver')) || has_role('supervisor', assignment_org = get_user_assignment_org(session[:authenticated_user],'supervisor')) ) && assignment_org&.root_org_setting('enable_workflows') == true
