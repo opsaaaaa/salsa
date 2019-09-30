1c8e95cda4c37fc1b5ee1fdfe0732962c77d5bcf
opsaaaaa <sean@ferney.org>
2019-09-30 09:57:03 -0600

    added or the exact same time to same_time?

--- app/helpers/time_zone_helper.rb
+++ app/helpers/time_zone_helper.rb
@@ -24,7 +24,7 @@ module TimeZoneHelper
     end
 
     def same_time? time1, time2, range = 1.minute
-        return (time1 - time2) < range
+        return time1 == time2 || (time1 - time2) < range
     end
 
 end
