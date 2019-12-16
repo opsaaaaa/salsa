314c1ed7e83c1360533167a46dca3ca711c8a139
opsaaaaa <sean@ferney.org>
2019-12-16 12:40:06 -0700

    cleanup

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -175,11 +175,10 @@ class DocumentsController < ApplicationController
 
     if @lms_course
       # see if there is a organization matched for course
-      @lms_course['account_id'] = "not cow"
       if @organization.setting('redirect_by_lms_account_id') && @lms_course['account_id'] && @organization.lms_account_id.to_s != @lms_course['account_id'].to_s 
-        lms_account_path = lms_account_course_document_path
-        if !lms_account_path.nil?
-          return redirect_to lms_account_course_document_path
+        org_by_lms_account = get_org_by_lms_account_id
+        if !org_by_lms_account.blank?
+          return redirect_to lms_account_course_document_path org_by_lms_account
         end
       end
       
