c5865a29c36922c34a582ea38107f05ea9e30433
opsaaaaa <sean@ferney.org>
2020-04-29 20:25:22 -0600

    cleanup

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -320,12 +320,12 @@ class DocumentsController < ApplicationController
     end
     respond_to do |format|
       if can_use_edit_token(@document.lms_course_id) != true
-        msg = { :status => "error", :message => "You do not have permisson to save this document"}
+        msg = { :status => "error", :message => "You do not have permission to save this document"}
       elsif republishing
-       msg = { :status => "error", :message => "Documents for this organization are currently being republished. Please copy your changes and try again later.", :version => @document.versions.count }
-     elsif @organization.root_org_setting("track_meta_info_from_document") == false && meta_data_from_doc != nil
+        msg = { :status => "error", :message => "Documents for this organization are currently being republished. Please copy your changes and try again later.", :version => @document.versions.count }
+      elsif @organization.root_org_setting("track_meta_info_from_document") == false && meta_data_from_doc != nil
         msg = { :status => "error", :message => "Tried to save document meta when document meta not enabled for this organization", :version => @document.versions.count }
-     elsif !saved && !meta_data_from_doc_saved
+      elsif !saved && !meta_data_from_doc_saved
         msg = { :status => "error", :message => "This is not a current version of this document! Please copy your changes and refresh the page to get the current version.", :version => @document.versions.count }
       else
         msg = { :status => "ok", :message => "Success!", :version => @document.versions.count }
